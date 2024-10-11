bed7a8a290f9caa9bccbc4d73cfd17815a136a30
Jayram Sharma <147648366+jayram0402@users.noreply.github.com>
2024-10-11 11:16:20 +0530

    Basic initializr app in working state

--- tinder-ai-backend/src/test/java/io/javabrains/tinderaibackend/TinderAiBackendApplicationTests.java
+++ tinder-ai-backend/src/test/java/io/javabrains/tinderaibackend/TinderAiBackendApplicationTests.java
@@ -1,6 +1,6 @@
 package io.javabrains.tinderaibackend;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
